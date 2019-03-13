d544915d04c0edb9dd92a9f0c529ab60fb555f8e
Hodos Adrian <adi.hodos@gmail.com>
2019-03-13 17:48:26 +0200

    Perlin noise implementation

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ mod hitable_list;
 mod lambertian;
 mod material;
 mod metal;
+mod perlin;
 mod ray;
 mod sphere;
 mod timer;
@@ -163,143 +164,167 @@ fn random_scene() -> (HitableList, Vec<Arc<Material>>) {
 const THREAD_COUNT: i32 = 4;
 const WORK_TILE_SIZE: u32 = 4;
 
-fn main() {
-  let nx = 1200;
-  let ny = 800;
-  let ns = 10;
+fn test_perlin() {
+  const IMG_WIDTH: u32 = 1024;
+  const IMG_HEIGHT: u32 = 1024;
+  const FEATURE_SIZE: u32 = 128;
 
-  let window = Window::new(0, nx, 0, ny);
-  let lookfrom = Vec3::new(13f32, 2f32, 3f32);
-  let lookat = Vec3::new(0f32, 0f32, 0f32);
-  let dist_to_focus = 10f32;
-  let aperture = 0.1f32;
+  let mut pixels: Vec<RGB8> = Vec::new();
 
-  let cam = Camera::new(
-    lookfrom,
-    lookat,
-    Vec3::new(0f32, 1f32, 0f32),
-    20f32,
-    nx as f32 / ny as f32,
-    aperture,
-    dist_to_focus,
-  );
+  let noise_gen = perlin::SimplexNoise::new();
 
-  let (world, _) = random_scene();
-  let world = Arc::new(world);
+  for y in 0..IMG_HEIGHT {
+    for x in 0..IMG_WIDTH {
+      let s = x as f32 / FEATURE_SIZE as f32;
+      let t = y as f32 / FEATURE_SIZE as f32;
 
-  let domains = {
-    let mut d = Vec::new();
-    let work_x = window.width() / WORK_TILE_SIZE;
-    let work_y = window.height() / WORK_TILE_SIZE;
-
-    println!("Work_x {} :: Work_y {}", work_x, work_y);
-
-    for y in 0..WORK_TILE_SIZE {
-      for x in 0..WORK_TILE_SIZE {
-        d.push(Window::new(
-          x * work_x,
-          (x + 1) * work_x,
-          y * work_y,
-          (y + 1) * work_y,
-        ));
-      }
-    }
-
-    println!("Work packages {}", d.len());
-    Arc::new(Mutex::new(d))
-  };
-
-  let (tx, rx) = mpsc::channel();
-
-  let mut threads = Vec::new();
-
-  let tmr = BasicTimer::new();
-
-  for i in 0..THREAD_COUNT {
-    let work_packages = domains.clone();
-    let tx = tx.clone();
-    let world = world.clone();
-
-    let thread = thread::spawn(move || loop {
-      let mut rng = thread_rng();
-
-      let current_work_package = {
-        let mut work_queue = work_packages.lock().unwrap();
-        work_queue.pop()
-      };
-
-      if !current_work_package.is_some() {
-        println!("Thread {} out of work, shutting down", i);
-        break;
-      }
-
-      let current_work_package = current_work_package.unwrap();
-
-      let mut pixels = Vec::new();
-
-      for y in current_work_package.ymin..current_work_package.ymax {
-        for x in current_work_package.xmin..current_work_package.xmax {
-          let mut col = Vec3::same(0f32);
-
-          for _ in 0..ns {
-            let dx: f32 = rng.gen();
-            let u = (x as f32 + dx) / nx as f32;
-
-            let dy: f32 = rng.gen();
-            let v = (y as f32 + dy) / ny as f32;
-
-            let r = cam.ray_at(u, v);
-            col += color(&r, &world, 0);
-          }
-
-          col /= ns as f32;
-          col = Vec3::new(col.x.sqrt(), col.y.sqrt(), col.z.sqrt());
-          let pixel_color = to_rgb8(col);
-          pixels.push(pixel_color);
-        }
-      }
-
-      tx.send((i, current_work_package, pixels)).unwrap();
-    });
-    threads.push(thread);
-  }
-
-  for t in threads {
-    t.join().unwrap();
-  }
-
-  tmr.end();
-  println!(
-    "Raytraced using {} threads, total time {} seconds",
-    THREAD_COUNT,
-    tmr.elapsed_seconds()
-  );
-
-  std::mem::drop(tx);
-
-  let mut image_pixels = Vec::new();
-  image_pixels.resize((nx * ny) as usize, RGB8::new(0, 0, 0));
-
-  for (tid, wpkg, pixels) in rx {
-    println!("Merging work package {:?} from thread {}", wpkg, tid);
-
-    // let fname = format!(
-    //     "wk_{}_{}_{}_{}.png",
-    //     wpkg.xmin, wpkg.xmax, wpkg.ymin, wpkg.ymax
-    // );
-
-    // write_image(&fname, wpkg.width() as u32, wpkg.height() as u32, &pixels)
-    //     .expect("Failed to write file!");
-
-    let mut idx = 0;
-    for y in wpkg.ymin..wpkg.ymax {
-      for x in wpkg.xmin..wpkg.xmax {
-        image_pixels[((ny - y - 1) * nx + x) as usize] = pixels[idx];
-        idx += 1;
-      }
+      let pixel = ((noise_gen.noise(s, t) + 1f32) * 127.5f32) as u8;
+      pixels.push(RGB8::new(pixel, 0, pixel));
     }
   }
 
-  write_image("raytraced.png", nx as u32, ny as u32, &image_pixels)
+  write_image("simplex.noise.png", IMG_WIDTH, IMG_HEIGHT, &pixels)
     .expect("Failed to write image!");
+}
+
+fn main() {
+  test_perlin();
+  // let nx = 1200;
+  // let ny = 800;
+  // let ns = 10;
+
+  // let window = Window::new(0, nx, 0, ny);
+  // let lookfrom = Vec3::new(13f32, 2f32, 3f32);
+  // let lookat = Vec3::new(0f32, 0f32, 0f32);
+  // let dist_to_focus = 10f32;
+  // let aperture = 0.1f32;
+
+  // let cam = Camera::new(
+  //   lookfrom,
+  //   lookat,
+  //   Vec3::new(0f32, 1f32, 0f32),
+  //   20f32,
+  //   nx as f32 / ny as f32,
+  //   aperture,
+  //   dist_to_focus,
+  // );
+
+  // let (world, _) = random_scene();
+  // let world = Arc::new(world);
+
+  // let domains = {
+  //   let mut d = Vec::new();
+  //   let work_x = window.width() / WORK_TILE_SIZE;
+  //   let work_y = window.height() / WORK_TILE_SIZE;
+
+  //   println!("Work_x {} :: Work_y {}", work_x, work_y);
+
+  //   for y in 0..WORK_TILE_SIZE {
+  //     for x in 0..WORK_TILE_SIZE {
+  //       d.push(Window::new(
+  //         x * work_x,
+  //         (x + 1) * work_x,
+  //         y * work_y,
+  //         (y + 1) * work_y,
+  //       ));
+  //     }
+  //   }
+
+  //   println!("Work packages {}", d.len());
+  //   Arc::new(Mutex::new(d))
+  // };
+
+  // let (tx, rx) = mpsc::channel();
+
+  // let mut threads = Vec::new();
+
+  // let tmr = BasicTimer::new();
+
+  // for i in 0..THREAD_COUNT {
+  //   let work_packages = domains.clone();
+  //   let tx = tx.clone();
+  //   let world = world.clone();
+
+  //   let thread = thread::spawn(move || loop {
+  //     let mut rng = thread_rng();
+
+  //     let current_work_package = {
+  //       let mut work_queue = work_packages.lock().unwrap();
+  //       work_queue.pop()
+  //     };
+
+  //     if !current_work_package.is_some() {
+  //       println!("Thread {} out of work, shutting down", i);
+  //       break;
+  //     }
+
+  //     let current_work_package = current_work_package.unwrap();
+
+  //     let mut pixels = Vec::new();
+
+  //     for y in current_work_package.ymin..current_work_package.ymax {
+  //       for x in current_work_package.xmin..current_work_package.xmax {
+  //         let mut col = Vec3::same(0f32);
+
+  //         for _ in 0..ns {
+  //           let dx: f32 = rng.gen();
+  //           let u = (x as f32 + dx) / nx as f32;
+
+  //           let dy: f32 = rng.gen();
+  //           let v = (y as f32 + dy) / ny as f32;
+
+  //           let r = cam.ray_at(u, v);
+  //           col += color(&r, &world, 0);
+  //         }
+
+  //         col /= ns as f32;
+  //         col = Vec3::new(col.x.sqrt(), col.y.sqrt(), col.z.sqrt());
+  //         let pixel_color = to_rgb8(col);
+  //         pixels.push(pixel_color);
+  //       }
+  //     }
+
+  //     tx.send((i, current_work_package, pixels)).unwrap();
+  //   });
+  //   threads.push(thread);
+  // }
+
+  // for t in threads {
+  //   t.join().unwrap();
+  // }
+
+  // tmr.end();
+  // println!(
+  //   "Raytraced using {} threads, total time {} seconds",
+  //   THREAD_COUNT,
+  //   tmr.elapsed_seconds()
+  // );
+
+  // std::mem::drop(tx);
+
+  // let mut image_pixels = Vec::new();
+  // image_pixels.resize((nx * ny) as usize, RGB8::new(0, 0, 0));
+
+  // for (tid, wpkg, pixels) in rx {
+  //   println!("Merging work package {:?} from thread {}", wpkg, tid);
+
+  //   // let fname = format!(
+  //   //     "wk_{}_{}_{}_{}.png",
+  //   //     wpkg.xmin, wpkg.xmax, wpkg.ymin, wpkg.ymax
+  //   // );
+
+  //   // write_image(&fname, wpkg.width() as u32, wpkg.height() as u32, &pixels)
+  //   //     .expect("Failed to write file!");
+
+  //   let mut idx = 0;
+  //   for y in wpkg.ymin..wpkg.ymax {
+  //     for x in wpkg.xmin..wpkg.xmax {
+  //       image_pixels[((ny - y - 1) * nx + x) as usize] = pixels[idx];
+  //       idx += 1;
+  //     }
+  //   }
+  // }
+
+  // write_image("raytraced.png", nx as u32, ny as u32, &image_pixels)
+  //   .expect("Failed to write image!");
 }
